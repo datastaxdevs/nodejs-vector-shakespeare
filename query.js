5c65b0d668d9e03cc4431fe5f44fe429ccf4e085
synedra-datastax <synedra@gmail.com>
2023-09-14 08:11:24 -0700

    Finished shakespeare example

--- query.js
+++ query.js
@@ -1,6 +1,7 @@
 'use strict';
 import OpenAI from "openai";
 import dotenv from 'dotenv';
+import json from 'json';
 
 dotenv.config();
 
@@ -22,23 +23,35 @@ const aclient = new cassandra.Client({
 
 
 async function run(c) {
-    let query_quote = "How did Juliet die?"
+    let query_quote = process.argv[3]
     let result = await openai.embeddings.create({
         model: "text-embedding-ada-002",
         input: query_quote
     })
     let embedding = new Float32Array(result.data[0].embedding)
 
-    console.log(embedding)
+    const query = "SELECT playerline, player, embedding_vector FROM vector.shakespeare_cql ORDER BY embedding_vector ANN OF ? LIMIT ?;"
+    const result_set = await aclient.execute(query, [embedding, 100], { prepare: true })
 
-    const query = "SELECT playerline, player, embedding_vector FROM vector.shakespeare_cql ORDER BY embedding_vector ANN OF ? LIMIT 1;"
-    const result_rows = await aclient.execute(query, embedding, { prepare: true })
+    let completion_model_name = "gpt-3.5-turbo"
 
-    for (let result_row in result_rows) {
-        console.log("HERE")
-        console.log(result_row)
+    let prompt = "Generate an answer to a question. Use only the information in the provided documents. If you don't know, just say you don't know, don't try to make up an answer. Answer with " + process.argv[2] + " words.\n\n"
+    prompt += "REFERENCE TOPIC: \n" + query_quote + "\n\n"
+    prompt += "ACTUAL EXAMPLES: \n"
+    for (let row of result_set.rows) {
+        prompt += " - " + row.playerline + "\n"
     }
 
+    console.log(prompt)
+
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: prompt }],
+        model: completion_model_name,
+        temperature: 0.7,
+        max_tokens: 320,
+    });
+
+    console.log(JSON.stringify(completion.choices[0].message.content))
     await c.shutdown();
 }
 
